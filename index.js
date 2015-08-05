76d392a06abf704c3fa98ededaf2ecfcf0f57ffb
Christian Petersen <humanoidism@gmail.com>
2015-08-05 14:09:43 +0200

    Follow Javascript Standard Style rules

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-'use strict';
-
 module.exports = function () {
-  return false;
-};
+  return false
+}
 
 module.exports.async = function (cb) {
   setTimeout(function () {
     if (typeof cb === 'function') {
       if (true === false) {
-        return cb(new Error('true cannot be false.'));
+        return cb(new Error('true cannot be false.'))
       }
 
-      return cb(null, false);
+      return cb(null, false)
     }
-  }, 0);
-};
+  }, 0)
+}
