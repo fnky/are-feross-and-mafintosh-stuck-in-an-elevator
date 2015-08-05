c1550526d769a6e18a6edffeca7df7615f227ee6
Christian Petersen <humanoidism@gmail.com>
2015-08-05 14:06:35 +0200

    Add asynchronous function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+'use strict';
+
 module.exports = function () {
-  return false // we've been rescued!
-}
+  return false;
+};
+
+module.exports.async = function (cb) {
+  setTimeout(function () {
+    if (typeof cb === 'function') {
+      if (true === false) {
+        return cb(new Error('true cannot be false.'));
+      }
+
+      return cb(null, false);
+    }
+  }, 0);
+};
